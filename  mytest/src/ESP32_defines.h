4ab8708051fb704f88e068444198a72e71519481
dalmatianrex <paulilunn@gmail.com>
2019-09-13 09:00:17 +0100

    Initial commit

---
+++  mytest/src/ESP32_defines.h
@@ -0,0 +1,21 @@
+//https://github.com/LilyGO/TTGO-T-Beam
+
+#define SERIAL_0_SPEED 115200
+#define SERIAL_1_SPEED 9600
+#define SCK 5   
+#define MISO 19 
+#define MOSI 27 I
+#define SS 18  
+#define RST 14  
+#define DI0 26  
+#define LORA_BAND 868E6
+#define LORA_SYNC_WORD 0x10
+#define ON_BOARD_LED 14
+#define ON_BOARD_SWITCH 39
+#define GPS_TX 12
+#define GPS_RX 15
+#define GPS_RESOLUTION 8
+#define LOOP_DELAY 1000 * 5
+#define BATTERY_INPUT 35
+#define ANALOG_MAX_VALUE 4095
+#define MAX_BATTERY_VOLTAGE 3.3
---
+++  mytest/test/test_main.cpp
@@ -0,0 +1,111 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <vector>
+#include <unity.h>
+#include <..\src\ESP32_defines.h>
+#include <TinyGPS++.h>
+
+// void setUp(void) {
+// // set stuff up here
+// }
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+void test_on_board_led(void)
+{
+    TEST_ASSERT_EQUAL(14, ON_BOARD_LED);
+}
+
+void test_led_state_high(void)
+{
+    digitalWrite(ON_BOARD_LED, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(ON_BOARD_LED));
+}
+
+void test_led_state_low(void)
+{
+    digitalWrite(ON_BOARD_LED, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(ON_BOARD_LED));
+}
+
+void test_i2c_bus(void)
+{
+    std::vector<int> i2c_results;
+    Wire.begin();
+    for (byte i = 0; i < 50; i++)
+    {
+        Wire.beginTransmission(i);
+        if (Wire.endTransmission() == 0)
+        {
+            i2c_results.push_back(i);
+        }
+    }
+    int num_i2c_found = i2c_results.size();
+    TEST_ASSERT_EQUAL(0, num_i2c_found);
+}
+
+void test_battery(void)
+{
+    int analogValue = analogRead(BATTERY_INPUT);
+    float voltage = analogValue * (MAX_BATTERY_VOLTAGE / ANALOG_MAX_VALUE) * 2.0;
+    TEST_ASSERT_TRUE(voltage > 0.0);
+    TEST_ASSERT_TRUE(voltage < (MAX_BATTERY_VOLTAGE * 2.0));
+}
+
+void test_gps(void)
+{
+    TinyGPSPlus gps;
+    double latitude = 0.0;
+    double longitude = 0.0;
+    int numSatellites = 0;
+    Serial1.begin(SERIAL_1_SPEED, SERIAL_8N1, GPS_TX, GPS_RX);
+
+    while (Serial1.available())
+    {
+        gps.encode(Serial1.read());
+    }
+
+    numSatellites = gps.satellites.value();
+
+    if (numSatellites > 0)
+    {
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+
+        Serial.print("Satellites: ");
+        Serial.println(numSatellites);
+        Serial.print("Latitude  : ");
+        Serial.println(latitude, 8);
+        Serial.print("Longitude : ");
+        Serial.println(longitude, 8);
+        Serial.println("**********************");
+    }
+    else
+    {
+        Serial.print("No satalit    tes found!")
+    }
+    
+}
+
+void setup()
+{
+    // NOTE!!! Wait for >2 secs
+    delay(2000);
+    pinMode(ON_BOARD_LED, OUTPUT);
+
+    UNITY_BEGIN(); // IMPORTANT LINE!
+
+    RUN_TEST(test_on_board_led);
+    RUN_TEST(test_led_state_high);
+    RUN_TEST(test_led_state_low);
+    RUN_TEST(test_i2c_bus);
+    RUN_TEST(test_battery);
+    RUN_TEST(test_gps);
+    UNITY_END(); // stop unit testing
+}
+
+void loop()
+{
+}
